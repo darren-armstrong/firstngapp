f186c055d2b379a43ba8de64ec6e8c15fa54d22e
Darren Armstrong <armstrong.d1990@gmail.com>
2017-06-14 13:28:31 +0100

    Angularjs2 tutorials added people component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {PeopleModule} from './people/people.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PeopleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
